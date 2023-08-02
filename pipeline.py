f85487d7ba1b96d1779e7b48774ca0752a2528a8
codingperks <r.perkins925@gmail.com>
2023-08-02 22:45:10 +0100

    Replaced argparse with constructor

---
+++ pipeline.py
@@ -0,0 +1,18 @@
+# to fill in
+# Joining together data pipeline and model pipeline
+
+
+class Pipeline:
+    def __init__(self, processor, model):
+        self._processor = processor
+        self._model = model
+
+    def set_dirs(self, data_dir, checkpoint_dir, inf_dir):
+        return
+
+    def train(config):
+        # big parse of config file
+        return
+
+    def inference(config):
+        return

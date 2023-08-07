881697261452202941fa60b6446d8e1e430b6c9a
codingperks <r.perkins925@gmail.com>
2023-08-07 11:09:23 +0100

    Added CLI param config to main

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
+import shutil
+from datetime import date
+
 from config.config import parse_json
-from data.audiocaps.ac_pipeline import ACPipeline
-from data.data_pipeline import WavPreprocessor
 from data.utils.spectrogram_params import SpectrogramParams
+
+# from data.ac_data_pipeline import ACPipeline
+from data.wav_preprocessor import WavPreprocessor
 from model.sd_ex.lora.train_text_to_image_lora import Sd_model_lora
 
 if __name__ == "__main__":
@@ -16,15 +20,40 @@ if __name__ == "__main__":
     )
     processor = WavPreprocessor(spectrogram_params=params)
 
-    # Prepare data
-    pipeline = ACPipeline("./data/audiocaps/dataset", processor)
-    pipeline.create_dataset(44100)
+    # Prepare data - uncomment if needed
+    # pipeline = ACPipeline("./data/audiocaps/dataset", processor)
+    # pipeline.create_dataset(44100)
 
     # Parse JSON parameters
     config = parse_json("config/json/config.json")
 
+    # CLI JSON Parsing
+    print("Select parameters")
+    lr = input("Learning rate: ")
+    steps = input("Training steps: ")
+    warmup = input("Warmup steps: ")
+
+    # Update config parameters
+    today = date.today()
+    date = today.strftime("%m-%d")
+    config["output_dir"] += date + "/" + lr + "_" + steps + "steps_" + warmup + "warmup"
+
+    if lr != "":
+        config["learning_rate"] = float(lr)
+    if steps != "":
+        config["max_train_steps"] = int(steps)
+    if warmup != "":
+        config["lr_warmup_steps"] = int(warmup)
+
+    print(f"Learning rate:{config['learning_rate']}")
+    print(f"Training steps: {config['max_train_steps']}")
+    print(f"Warmup steps: {config['lr_warmup_steps']}")
+
     # Create model
     model = Sd_model_lora(preprocessor=processor, **config)
+
+    # Save config in output folder
+    shutil.copy("config/json/config.json", config["output_dir"])
 
     # train model
     model.train()

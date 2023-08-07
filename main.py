911e5340597b4625d7ed7564738e5995d49964fb
Ryan Perkins <rp22@kanye.doc.ic.ac.uk>
2023-08-07 20:49:42 +0100

    fixing imperial merge conflicts

--- main.py
+++ main.py
@@ -1,13 +1,61 @@
-from config.config import parse_json
-from data.data_pipeline import WavPreprocessor
-from data.utils.spectrogram_params import SpectrogramParams
-from model.sd_ex.lora.train_text_to_image_lora import Sd_model_lora
+#from config.config import parse_json
+#from data.data_pipeline import WavPreprocessor
+#from data.utils.spectrogram_params import SpectrogramParams
+#from model.sd_ex.lora.train_text_to_image_lora import Sd_model_lora
+from data.audiocaps.downloader import download_audiocaps
+import os
 
 if __name__ == "__main__":
     # Prepare data
     # add when needed
 
-    # Create processor
+    """ 
+    path = 'data/audiocaps/dataset/train/'
+    files = [f for f in os.listdir(path) if os.path.isfile(os.path.join(path, f))]
+    number_of_files = len(files)
+
+    print(f"There are {number_of_files} files in the train folder.")
+
+    path = 'data/audiocaps/dataset/test/'
+    files = [f for f in os.listdir(path) if os.path.isfile(os.path.join(path, f))]
+    number_of_files = len(files)
+
+    print(f"There are {number_of_files} files in the test folder.")
+
+    path = 'data/audiocaps/dataset/val/'
+    files = [f for f in os.listdir(path) if os.path.isfile(os.path.join(path, f))]
+    number_of_files = len(files)
+
+    print(f"There are {number_of_files} files in the val folder.") """
+
+    """      download_audiocaps(
+            '/vol/bitbucket/rp22/sdspeech/data/audiocaps/dataset/train.csv',
+            '/vol/bitbucket/rp22/sdspeech/data/audiocaps/dataset/train_download_success.csv',
+            '/vol/bitbucket/rp22/sdspeech/data/audiocaps/dataset/train_download_fail.csv',
+            '/vol/bitbucket/rp22/sdspeech/data/audiocaps/dataset/train',
+            0.1
+        ) """
+
+    # Validation dataset
+    download_audiocaps(
+        '/vol/bitbucket/rp22/sdspeech/data/audiocaps/dataset/val.csv',
+        '/vol/bitbucket/rp22/sdspeech/data/audiocaps/dataset/val_download_success.csv',
+        '/vol/bitbucket/rp22/sdspeech/data/audiocaps/dataset/val_download_fail.csv',
+        '/vol/bitbucket/rp22/sdspeech/data/audiocaps/dataset/val',
+        1.0
+    )
+
+    # Test dataset
+    download_audiocaps(
+        '/vol/bitbucket/rp22/sdspeech/data/audiocaps/dataset/test.csv',
+        '/vol/bitbucket/rp22/sdspeech/data/audiocaps/dataset/test_download_success.csv',
+        '/vol/bitbucket/rp22/sdspeech/data/audiocaps/dataset/test_download_fail.csv',
+        '/vol/bitbucket/rp22/sdspeech/data/audiocaps/dataset/test',
+        1.0
+    )
+    
+    
+    """     # Create processor
     params = SpectrogramParams(
         sample_rate=44100,
         stereo=False,
@@ -26,3 +74,4 @@ if __name__ == "__main__":
 
     # train model
     model.train()
+    """

402d7a7b0c12d8a12c834fe0821fa2250735b0a0
Ryan <56696610+codingperks@users.noreply.github.com>
2023-08-18 16:46:26 +0100

    Rework/batch size investigation (#17)
    * Adjusted main.py to allow for batch size inputation
    
    * Adjusted training script to allow for higher batches

--- main.py
+++ main.py
@@ -33,24 +33,28 @@ if __name__ == "__main__":
         description="Train the model with custom parameters."
     )
     parser.add_argument(
-        "--lr", type=float, help="Learning rate",
-        default=config["learning_rate"]
+        "--lr", type=float, help="Learning rate", default=config["learning_rate"]
     )
     parser.add_argument(
-        "--steps", type=int, help="Training steps",
-        default=config["max_train_steps"]
+        "--steps", type=int, help="Training steps", default=config["max_train_steps"]
     )
     parser.add_argument(
-        "--warmup", type=int, help="Warmup steps",
-        default=config["lr_warmup_steps"]
+        "--warmup", type=int, help="Warmup steps", default=config["lr_warmup_steps"]
     )
     parser.add_argument(
-        "--adamw", type=float, help="Adam weight decay",
-        default=config["adam_weight_decay"]
+        "--adamw",
+        type=float,
+        help="Adam weight decay",
+        default=config["adam_weight_decay"],
     )
     parser.add_argument(
-        "--adam2", type=float, help="Adam b2",
-        default=config["adam_beta2"]
+        "--adam2", type=float, help="Adam b2", default=config["adam_beta2"]
+    )
+    parser.add_argument(
+        "--batch",
+        type=int,
+        help="Train and val batch sizes",
+        default=config["train_batch_size"],
     )
 
     args = parser.parse_args()
@@ -62,21 +66,26 @@ if __name__ == "__main__":
         "output_dir"
     ] += f"{date_str}/{args.lr}_{args.steps}steps_{args.warmup}warmup"
     config_dir = config["output_dir"].replace(".", "")
-    
+
     config["learning_rate"] = args.lr
     config["max_train_steps"] = args.steps
     config["lr_warmup_steps"] = args.warmup
-    
+
     if args.adamw:
         config["adam_weight_decay"] = args.adamw
     if args.adam2:
         config["adam_beta2"] = args.adam2
+    if args.batch:
+        config["train_batch_size"] = args.batch
+        config["val_batch_size"] = args.batch
 
     print(f"Learning rate:{config['learning_rate']}")
     print(f"Training steps: {config['max_train_steps']}")
     print(f"Warmup steps: {config['lr_warmup_steps']}")
     print(f"Adam weight decay: {config['adam_weight_decay']}")
     print(f"Adam beta 2: {config['adam_beta2']}")
+    print(f"Train batch size: {config['train_batch_size']}")
+    print(f"Val batch size: {config['val_batch_size']}")
 
     # Create model
     model = Sd_model_lora(preprocessor=processor, **config)

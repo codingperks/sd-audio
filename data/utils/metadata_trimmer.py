c419f4ad4cdf6e19a8c74ea256887ab9f567a18c
Ryan Perkins <rp22@springroll.doc.ic.ac.uk>
2023-08-28 18:45:33 +0100

    removed NSFW safety checker

---
+++ data/utils/metadata_trimmer.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+# Remove 'a spectrogram of' from all metadata  
+def trim_metadata_captions(file_path):
+    # Read the CSV into a pandas DataFrame
+    df = pd.read_csv(file_path)
+
+    # Remove the substring from the 'text' column
+    df['text'] = df['text'].str.replace('a spectrogram of', '', regex=False)
+    df['text'] = df['text'].str.replace('"', '', regex=False)
+
+    # Save the modified DataFrame back to the CSV
+    df.to_csv(file_path, index=False)
